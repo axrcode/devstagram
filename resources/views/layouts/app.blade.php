bce22171cf905815a2011c5200a1e27a2aac196d
axrcode <axrcode@gmail.com>
2022-05-16 21:47:12 -0600

    Add layout principal

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,6 +9,37 @@
     <script src="{{ asset('js/app.js') }}"></script>
 </head>
 <body class="bg-gray-200">
-    @yield('content')
+
+    <header class="p-5 border-b bg-white shadow">
+        <div class="container mx-auto flex justify-between items-center">
+            <h1 class="text-3xl font-black">
+                DevStagram
+            </h1>
+
+            <nav class="flex gap-2">
+                <a  href="" 
+                    class="font-bold uppercase text-gray-600">
+                    Login
+                </a>
+                <a  href="{{ route('register') }}" 
+                    class="font-bold uppercase text-gray-600">
+                    Crear Cuenta
+                </a>
+            </nav>
+        </div>
+    </header>
+
+    <main class="container mx-auto mt-10">
+        <h2 class="font-black text-center text-3xl mb-10">
+            @yield('title')
+        </h2>
+
+        @yield('content')
+    </main>
+    
+    <footer class="text-center p-5 text-gray-500 font-bold uppercase">
+        DevStagram - Todos los derechos reservados &copy; {{ now()->year }}
+    </footer>
+
 </body>
 </html>
